89abff0a4117ea97528f17ddbc351f0b2225b8ca
wuyiping0628 <1106773985@qq.com>
2018-10-31 21:43:51 +0800

    index 静态图形完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -9,9 +9,34 @@ $(document).ajaxStop(function () {
 });
 
 
-// 二级导航隐藏设置
+// 1 二级导航隐藏设置
 $('.lt_aside .nav .category').click(function() {
   $(this).next().stop().slideToggle();
 });
 
-// 导航高亮效果
+// 2 左侧菜单的切换
+$('.lt_topbar .icon_menu').click(function(){
+  $('.lt_aside').toggleClass("hidemenu");
+  $('.lt_main').toggleClass("hidemenu");
+  $('.lt_topbar').toggleClass("hidemenu");
+
+})
+
+// 3. 退出功能的实现
+$('.lt_topbar .icon_logout').click(function(){
+  $('#logoutModal').modal("show");
+})
+
+$('#logoutBtn').click(function(){
+  $.ajax({
+    type:"get",
+    url:'/employee/employeeLogout',
+    dateType:'json',
+    success:function(info){
+      // console.log(info);
+      if(info.success){
+        location.href="login.html";
+      }
+    }
+  })
+})
